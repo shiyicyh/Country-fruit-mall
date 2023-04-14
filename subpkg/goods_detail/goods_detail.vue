c6474b63472584b9625af4d12b093f2cd8f5c97f
cyhNO.1 <yuehuachen11@163.com>
2023-04-14 10:47:08 +0800

    商品详情页完成

--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -7,24 +7,26 @@
       </swiper-item>
     </swiper>
     <!-- 商品信息区域 -->
-    <view class="goods-info-box">
+    <view class="goods-header-box box">
+      <!-- 标题、描述、价格 -->
       <!-- 商品价格 -->
-      <view class="price">￥{{goods_info.goods_price}}</view>
-      <!-- 信息主体区域 -->
-      <view class="goods-info-body">
-        <!-- 商品名称 -->
-        <view class="goods-name">{{goods_info.name}}</view>
-        <!-- 收藏 -->
-        <view class="favi">
-          <uni-icons type="star" size="18" color="gray"></uni-icons>
-          <text>收藏</text>
-        </view>
+      <h4>{{goods_info.name}}</h4>
+      <view class="desc">{{goods_info.goods_desc}}</view>
+      <view class="price">￥<text>{{goods_info.goods_price}}</text></view>
+      
+    </view>
+    
+    <view class="goods-info-box box">
+      <view>
+        <text class="info-title">配送</text><text class="info">包邮到家</text>
       </view>
-      <!-- 运费 -->
-      <view class="yf">快递：免运费</view>
+      <view>
+        <text class="info-title">规格</text><text class="info">{{goods_info.standard}}</text>
+      </view>
     </view>
     <!-- 商品详情信息 -->
-    <view class="goods-introduce-box">
+    <view class="goods-introduce-box box">
+      <view class="detail-text">商品详情</view>
       <image v-for="item in goods_info.goods_introduce_imgs" :src="item" mode="widthFix" class="introduce-img"></image>
     </view>
     <!-- <rich-text :nodes="goods_info.goods_introduce"></rich-text> -->
@@ -41,6 +43,7 @@
 </template>
 
 <script>
+  import {mapMutations,mapGetters} from 'vuex'
   const db = uniCloud.databaseForJQL();
   export default {
     data() {
@@ -50,11 +53,11 @@
         // 左侧按钮组的配置对象
         options: [{
           icon: 'shop',
-          text: '店铺'
+          text: '首页'
         }, {
           icon: 'cart',
           text: '购物车',
-          info: 2
+          info: 0,
         }],
         // 右侧按钮组的配置对象
         buttonGroup: [{
@@ -76,12 +79,19 @@
       // 调用请求商品详情数据的方法
       this.getGoodsDetail(goods_id)
     },
+    onShow(){
+       this.options[1].info=this.total
+    },
+    computed:{
+      ...mapGetters('m_cart', ['total']),
+    },
     methods: {
+      ...mapMutations('m_cart',['addToCart']),
       // 定义请求商品详情数据的方法
       getGoodsDetail(goods_id) {
         db.collection('opendb-mall-goods')
         	.where("_id=='"+goods_id+"'")
-          .field("goods_price,name,goods_swiper_imgs,goods_introduce_imgs")
+          .field("goods_desc,goods_price,name,goods_swiper_imgs,goods_introduce_imgs,standard,goods_thumb,remain_count")
         	.get()
         	.then((res)=>{
             // res 为数据库查询结果
@@ -114,7 +124,27 @@
               url: '/pages/cart/cart'
             })
         }
-      }
+        if (e.content.text === '首页') {
+            // 切换到购物车页面
+            uni.switchTab({
+              url: '/pages/home/home'
+            })
+        }
+      },
+      buttonClick(e){
+        if (e.content.text === '加入购物车') {
+            // 切换到购物车页面
+            this.addToCart(this.goods_info)
+            this.options[1].info=this.total
+        }
+        if (e.content.text === '立即购买') {
+            uni.showToast({
+              title: '该模块待开发',
+              duration: 2000
+            });
+        }
+      },
+      
     }
   }
 </script>
@@ -124,6 +154,12 @@
   // 给页面外层的容器，添加 50px 的内padding，
   // 防止页面内容被底部的商品导航组件遮盖
   padding-bottom: 50px;
+  background-color: #efefef;
+}
+.box{
+  margin: 7px;
+  border-radius: 5px;
+  background-color: #fff;
 }
 swiper {
   height: 750rpx;
@@ -134,46 +170,72 @@ swiper {
   }
 }
 // 商品信息区域的样式
-.goods-info-box {
-  padding: 10px;
+.goods-header-box {
+  padding: 17px;
   padding-right: 0;
-
+  .desc{
+    font-size: 14px;
+    color: gray;
+    padding: 10px 0;
+  }
   .price {
     color: #c00000;
-    font-size: 18px;
-    margin: 10px 0;
-  }
-
-  .goods-info-body {
-    display: flex;
-    justify-content: space-between;
-
-    .goods-name {
-      font-size: 13px;
-      padding-right: 10px;
-    }
-    // 收藏区域
-    .favi {
-      width: 120px;
-      font-size: 12px;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      border-left: 1px solid #efefef;
-      color: gray;
+    font-size: 14px;
+    text{
+      font-size: 20px;
     }
   }
+
+  // .goods-header-body {
+  //   display: flex;
+  //   justify-content: space-between;
+
+  //   .goods-name {
+  //     font-size: 16px;
+  //     padding-right: 10px;
+
+  //   }
+  //   // 收藏区域
+  //   .favi {
+  //     width: 120px;
+  //     font-size: 12px;
+  //     display: flex;
+  //     flex-direction: column;
+  //     justify-content: center;
+  //     align-items: center;
+  //     border-left: 1px solid #efefef;
+  //     color: gray;
+  //   }
+  // }
 
   // 运费
-  .yf {
-    margin: 10px 0;
-    font-size: 12px;
+  // .yf {
+  //   margin: 10px 0;
+  //   font-size: 12px;
+  //   color: gray;
+  // }
+}
+.goods-info-box{
+  font-size: 12px;
+  view{
+    padding: 8px 0;
+    line-height: 12px;
+  }
+  .info-title{
+    margin: 15px;
     color: gray;
+  }
+  .info{
+    color: #000;
   }
 }
 .goods-introduce-box{
-  width: 100%;
+  overflow: hidden;
+  .detail-text{
+    font-size: 13px;
+    margin: 10px;
+    font-weight: 600;
+  }
   .introduce-img{
     display: block; 
     width: 100%;
